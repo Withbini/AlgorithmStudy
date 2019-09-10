e4715f14d6a31aab2607ad537527fb78c5c5df52
Withbini <47143101+Withbini@users.noreply.github.com>
2019-09-10 17:02:13 +0900

    Create 15651.cpp

---
+++ study/15651.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include<cstdio>
+
+using namespace std;
+int arr[10];
+int visited[10];
+int n, m;
+void dfs(int x) {
+	if (x == m) {
+		for (int i = 0; i < m; i++) {
+			printf("%d ", arr[i]);
+		}
+		printf("\n");
+		return;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		if (visited[x] == -1) {
+			visited[x] = 1;
+			arr[x] = i;
+			dfs(x + 1);
+			visited[x] = -1;
+		}
+	}
+}
+
+int main() {
+	fill(visited, visited + sizeof(visited) / sizeof(int), -1);
+	scanf("%d %d", &n, &m);
+	dfs(0);
+	return 0;
+}
